e5b1b330c91cb4642df208b093216378c6d3fa9b
laihuanmin <lai_huanmin@163.com>
2018-03-13 22:46:28 +0800

    first init

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+
+#define ListSize 100
+typedef int DataType;
+typedef struct {
+    DataType data[ListSize];
+    int length;
+} SeqList;
+
+
+int main() {
+    SeqList seqList;
+    seqList.length = 0;
+//    insertList(seqList, 1, 200);
+    printf("%d", seqList.length);
+    return 0;
+}
+
+void removeList(SeqList *seqList, int index) {
+    if (index < 0 || index > seqList->length - 1) {
+        printf("you are wrong");
+    }
+    DataType k;
+    for (k = index; k < seqList->length - 2; k++) {
+        seqList->data[k] = seqList->data[k + 1];
+    }
+    seqList->length--;
+}
+
+void insertList(SeqList *seqList, int i, DataType j) {
+    if (i < 0 || i > (seqList->length + 1)) {
+        printf("error ");
+        return;
+    }
+    for (j = seqList->length - 1; j >= i - 1; j++) {
+        seqList->data[j + 1] = seqList->data[j];
+    }
+    seqList->data[i - 1] = j;
+    seqList->length++;
+}
